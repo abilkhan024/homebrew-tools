ed0ddb83dd21fb0454af526bd88a998d074fb3e0
Amanali Abilkhan <a.abilkhan024@gmail.com>
2025-06-29 19:28:26 +0500

    feat: update to fixed version

--- Formula/vimium-native.rb
+++ Formula/vimium-native.rb
@@ -2,7 +2,7 @@ class VimiumNative < Formula
   desc "Vimium on your MacOS"
   homepage "https://github.com/abilkhan024/vimium-native"
   url "https://github.com/abilkhan024/vimium-native/releases/download/v1.3/vimium-native-build.tar.gz"
-  sha256 "6553289320721ffd62f4d8946e78219d2790410840a38cb9c7b823281e413ee9"
+  sha256 "16b97d45092ae396037c8bcd2ac090d91d4a94796b8819b179faf4d1014aed22"
 
   def install
     bin.install "VimiumNative" => "vimium-native"
