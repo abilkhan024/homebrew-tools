8a23d71cfeeeef77a9ca586c87fa26490499983b
Amanali Abilkhan <a.abilkhan024@gmail.com>
2025-08-13 08:31:07 -0700

    feat: upgrade win-pin

--- Formula/win-pin.rb
+++ Formula/win-pin.rb
@@ -1,8 +1,8 @@
 class WinPin < Formula
   desc "Basic window manager for MacOS"
   homepage "https://github.com/abilkhan024/win-pin"
-  url "https://github.com/abilkhan024/win-pin/releases/download/v1.0/winpin-build.tar.gz"
-  sha256 "adfba8a8592af57bba0c62cf6e7dde7679b655f9391b3d71814e8356b5c16707"
+  url "https://github.com/abilkhan024/win-pin/releases/download/v1.1/winpin-build.tar.gz"
+  sha256 "01acb999ace09c83888fa2f2de47086cdb65f265058d29a96f166cfaca968b13"
 
   def install
     bin.install "WinPin" => "win-pin"
