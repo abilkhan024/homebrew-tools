813da4ad110abb805a224b555f975696e03fb295
Amanali Abilkhan <a.abilkhan024@gmail.com>
2025-10-20 16:01:14 +0300

    feat: change vimium version to the one with minor fix

--- Formula/vimium-native.rb
+++ Formula/vimium-native.rb
@@ -2,7 +2,7 @@ class VimiumNative < Formula
   desc "Vimium on your MacOS"
   homepage "https://github.com/abilkhan024/vimium-native"
   url "https://github.com/abilkhan024/vimium-native/releases/download/v1.8/vimium-native-build.tar.gz"
-  sha256 "465ee90766e1ba28c6815125125a7a1a384b7129cd0cc852f6ea24e37a27781c"
+  sha256 "302a4f2c0f53dd0e49fa803870a17b8457fb514ca122370cf58093f944ce664b"
 
   def install
     bin.install "VimiumNative" => "vimium-native"
