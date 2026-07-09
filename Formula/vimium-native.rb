7f0ffcf7d06a1ddf59e5f1a323d9c2a30d38a556
Amanali Abilkhan <a.abilkhan024@gmail.com>
2026-07-09 21:54:52 +0300

    feat: upgrade vimium-native to v1.9

--- Formula/vimium-native.rb
+++ Formula/vimium-native.rb
@@ -1,8 +1,8 @@
 class VimiumNative < Formula
   desc "Vimium on your MacOS"
   homepage "https://github.com/abilkhan024/vimium-native"
-  url "https://github.com/abilkhan024/vimium-native/releases/download/v1.8/vimium-native-build.tar.gz"
-  sha256 "302a4f2c0f53dd0e49fa803870a17b8457fb514ca122370cf58093f944ce664b"
+  url "https://github.com/abilkhan024/vimium-native/releases/download/v1.9/vimium-native-build.tar.gz"
+  sha256 "e3c53b0f3eda1a10731db534c467717589d59e6e3ac530d994b3671674462ff6"
 
   def install
     bin.install "VimiumNative" => "vimium-native"
